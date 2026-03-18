8f14565d33c1a7a73f73f79fac76a87bde7d85cc
Danilo Borges de Oliveira <130411929+danilo-oliv@users.noreply.github.com>
2026-03-18 23:29:04 +0000

    pratica

---
+++ pratica01/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+
+export default function HomeScreen() {
+    
+    return(
+        <View>
+            <Text>Bem-vindo ao aplicativo. Utilize o menu de navegação para acessar as telas de modais e as listas com rolagem</Text>
+        </View>
+    )
+    
+}
